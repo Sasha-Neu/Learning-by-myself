79b0ae3d0cd72d59924c3f77ad2f2692ca3805ca
Sasha <machete.pills@gmail.com>
2023-01-26 11:02:43 -0800

    First greeting

---
+++ Learning/Learning/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace Learning
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string myName = "Sasha";
+            
+            Console.WriteLine("Hello, my name is {0}!", myName);
+        }
+    }
+}
