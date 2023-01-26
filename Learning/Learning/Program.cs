7cea007252bc0a70d50471b9a1e050636632a821
Sasha <machete.pills@gmail.com>
2023-01-26 11:18:47 -0800

    Name and age displayed

--- Learning/Learning/Program.cs
+++ Learning/Learning/Program.cs
@@ -5,8 +5,11 @@
         static void Main(string[] args)
         {
             string myName = "Sasha";
+            decimal myAge = 23;
             
-            Console.WriteLine("Hello, my name is {0}!", myName);
+            Console.WriteLine("Hello, my name is {0,-10}! I am {1} years old.", myName, myAge);
+            Console.WriteLine("Hello! My name is " + myName + ". I am " + myAge + " years old.");
+            Console.WriteLine($"Hello! My name is {myName}. I am {myAge} years old.");
         }
     }
 }
